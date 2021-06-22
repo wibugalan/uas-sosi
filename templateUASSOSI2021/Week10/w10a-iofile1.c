0dd033e566eee11dfb14807bb2f1d2094eb429a8
muhammad.marsha <marshammp@gmail.com>
2021-06-22 16:52:01 +0700

    week10a first attempt

--- templateUASSOSI2021/Week10/w10a-iofile1.c
+++ templateUASSOSI2021/Week10/w10a-iofile1.c
@@ -12,8 +12,22 @@
 
 void main(void) {
    int n, off, ofd;
+   char text[100];
+   ofd = open (OFILE, O_RDWR | O_CREAT | O_TRUNC, 0644);
+   scanf("%d\n", &n);
 
+   for(int i = 0; i < n; i++){
+      char mod[1];
+      scanf("%s %d %d", text, &off, &mod);
+      int textsize = strlen(text);
+      if (mod == "A") {
+         lseek(ofd, off, SEEK_SET);
+         write(ofd, text, textsize);
+      }
+      else if (mod == "T") {
+         lseek(ofd, off, SEEK_SET);
+         write(ofd, text, textsize);
+      }
+   close(ofd);
    printf("Please see file %s\n", OFILE);
-
-
 }

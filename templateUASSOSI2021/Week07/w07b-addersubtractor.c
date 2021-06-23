a9282ce272a134fabfa35ef7fc7978ecd262ab23
samudrobintang10 <samudrobintang10@gmail.com>
2021-06-23 21:23:01 +0700

    7b sementara boi

--- templateUASSOSI2021/Week07/w07b-addersubtractor.c
+++ templateUASSOSI2021/Week07/w07b-addersubtractor.c
@@ -5,34 +5,70 @@
 #include <stdlib.h>
 #include "99-myutils.h"
 
+sem_t obs_sem;
+sem_t addsub_sem;
 
 #define T_REHAT 2000
 
+int n;
 int idmaster = 0;
 int sharedInt = 0;
-
-int randomNumber(){
-	int num = (rand() % (5-1+1)) + 1;
-	return angkaRandom;
-}
+int counter = 1;
+int var_add, var_sub;
 
 void* adder (void* a) {
-
+	sem_post(&obs_sem);
+	int id;
+	id = ++idmaster;
+	while(TRUE){
+		if (++counter == 2*n) {
+			sem_post(&obs_sem);
+			counter = 0;
+		}
+		//printf("%d dan %d", counter, 2*n);
+		fflush(NULL);
+		rehat_acak(T_REHAT);
+		sem_wait(&addsub_sem);
+		var_add = (rand()%5) + 1;
+		sharedInt += var_add;
+		printf("Thread=[%3.3d] ADD [%3.3d]  CURENT VALUE  [%3.3d]\n", id, var_add, sharedInt);
+		sem_post(&addsub_sem);
+	}
 }
 
 
 void* substractor (void* a) {
-
+	sem_post(&obs_sem);
+	int id;
+	id = ++idmaster;
+	while(TRUE){
+		if (++counter == 2*n) {
+			sem_post(&obs_sem);
+			counter = 0;
+		}
+		//printf("%d dan %d\n", counter, 2*n);
+		fflush(NULL);
+		rehat_acak(T_REHAT);
+		sem_wait(&addsub_sem);
+		var_sub = (rand()%5) + 1;
+		sharedInt -= var_sub;
+		printf("Thread=[%3.3d] SUB [%3.3d]  CURENT VALUE  [%3.3d]\n", id, var_sub, sharedInt);
+		sem_post(&addsub_sem);
+	}
 }
 
 
 void* observer (void* a) {
+	sem_wait(&obs_sem);
+	int id;
+	id = ++idmaster;
+	printf("Thread=[%3.3d] OBS ---  CURENT VALUE  [%3.3d]\n", id, sharedInt);
+	sem_post(&addsub_sem);
 	
 }
 
 int main(int argc, char * argv[])
 {  
-   int n;
 
 
    printf("Hello... \n");
@@ -43,7 +79,6 @@ int main(int argc, char * argv[])
    daftar_trit(adder);
    daftar_trit(substractor);
    daftar_trit(observer);
-
 
    jalankan_trit();
    beberes_trit("Selesai...");

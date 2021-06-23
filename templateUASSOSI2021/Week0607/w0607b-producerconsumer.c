946e8264bdce3a0f5eb6f8fb434151df3ede7531
muhammad.marsha <marshammp@gmail.com>
2021-06-23 20:55:29 +0700

    w0607b attempt

--- templateUASSOSI2021/Week0607/w0607b-producerconsumer.c
+++ templateUASSOSI2021/Week0607/w0607b-producerconsumer.c
@@ -28,27 +28,112 @@ typedef  struct {
 } myshare;
 
 myshare* mymap;
-int idmaster=0;
+int idmaster=1;
+int rr = 0;
 
 void* producer (void* a) {
+   int id;
+   sem_wait (&mutex);
+   id = idmaster++;
+   sem_post (&mutex);
 
+	for(;;){
+		rehat_acak(T_REHAT);
+		int random= (rand()%999)+1;
+		sem_wait (&(mymap->buffmutex));
+
+		mymap->buffsize = mymap->buffsize + 1; 
+      	if ((mymap->buffsize)>99){
+        	mymap->buffsize = 0;
+      	}
+      	(mymap->buffer)[mymap->buffsize - 1] = random;
+         rr = random;
+		printf("PID[%4.4d] \t Thread[%4.4d] \t PRODUCE %3.3d \t BUFFERSIZE [%4.4d]\n", 
+			getpid(),id,random,mymap->buffsize);
+
+		sem_post (&(mymap->buffmutex));
+	}
 }
 
 
 void* consumer (void* a) {
+   int id;
+   sem_wait (&mutex);
+   id = idmaster++;
+   sem_post (&mutex);
 
+	for(;;){
+		while((mymap->buffsize)<=0){
+			sleep(1);
+		}
+		rehat_acak(T_REHAT);
+		int random= (rand()%999)+1;
+      rr = random;
+		sem_wait (&(mymap->buffmutex));
+
+		(mymap->buffer)[mymap->buffsize - 1] = 0;
+		mymap->buffsize = mymap->buffsize - 1;
+		printf("PID[%4.4d] \t Thread[%4.4d] \t CONSUME %3.3d \t BUFFERSIZE [%4.4d]\n", 
+			getpid(),id, random, mymap->buffsize);
+
+		sem_post (&(mymap->buffmutex));
+	}
 }
 
 void* observer (void* a) {
+   int id;
+   sem_wait (&mutex);
+   id = idmaster++;
+   sem_post (&mutex);
 
+	for(;;){
+		while((mymap->buffsize)<=0){
+			sleep(1);
+		}
+		rehat_acak(T_REHAT);
+		int random= (rand()%999)+1;
+		sem_wait (&(mymap->buffmutex));
+
+		(mymap->buffer)[mymap->buffsize] = 0;
+		mymap->buffsize = mymap->buffsize - 1;
+		printf("PID[%4.4d] \t Thread[%4.4d] \t OBSERVE %3.3d \t BUFFERSIZE [%4.4d]\n", 
+			getpid(),id, rr, mymap->buffsize);
+
+		sem_post (&(mymap->buffmutex));
+	}
 }
 
 int main(int argc, char * argv[])
 {  
 
-   daftar_trit(producer);
-   daftar_trit(consumer);
-   daftar_trit(observer);
+   sem_init (&mutex,  0, 1);
+
+	int fd = open(SFILE,MYFLAGS,S_IRWXU);
+   	int ssize = sizeof(myshare);
+   	truncate(SFILE, ssize);
+   	mymap=mmap(NULL, ssize, MYPROTECT, MYVISIBILITY, fd, 0);
+   	mymap-> buffsize = 0;
+   	sem_init (&(mymap->buffmutex), 0, 1);
+
+	pid_t process = fork();
+	if (process>0){
+		if (process>0) {
+			for (int i = 0; i < NN; ++i){
+				daftar_trit(producer);
+			}
+		}
+		else {
+			for (int i = 0; i < NN-2; ++i){
+				daftar_trit(observer);
+			}
+		}
+		
+	}else{
+		for (int i = 0; i < NN; ++i){
+			daftar_trit(consumer);
+		}
+	}
+   
    jalankan_trit();
    beberes_trit("Selesai...");
 

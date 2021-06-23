3a2f32e0fadfbcb5ab25b1d2c8cf76adb63bcf65
samudrobintang10 <samudrobintang10@gmail.com>
2021-06-23 16:32:07 +0700

    Change on Week 7a

--- templateUASSOSI2021/Week07/w07a-writerreader.c
+++ templateUASSOSI2021/Week07/w07a-writerreader.c
@@ -10,37 +10,45 @@ sem_t sem_3;
 sem_t sem_4;
 
 void* T1 (void* a) {
-	sem_wait(&sem_4);
-	for(int ii = 1; ii < 2; ii = ii + 1) {
-		printf("T1     Writer        [%d]\n", ii);
+	while(TRUE) {
+		sem_wait(&sem_4);
+		for(int ii = 1; ii < 2; ii = ii + 1) {
+			printf("T1     Writer        [%d]\n", ii);
+		}
+		sem_post(&sem_1);
 	}
-	sem_post(&sem_1);
 }
 
 
 void* T2 (void* a) {
-	sem_wait(&sem_1);
-	for(int ii = 1; ii < 3; ii = ii + 1) {
-		printf("T2     Writer        [%d]\n", ii);
+	while(TRUE) {
+		sem_wait(&sem_1);
+		for(int ii = 1; ii < 3; ii = ii + 1) {
+			printf("T2     Writer        [%d]\n", ii);
+		}
+		sem_post(&sem_2);
 	}
-	sem_post(&sem_2);
 }
 
 
 void* T3 (void* a) {
-	sem_wait(&sem_2);
-	for(int ii = 1; ii < 4; ii = ii + 1) {
-		printf("T3     Reader        [%d]\n", ii);
+	while(TRUE) {
+		sem_wait(&sem_2);
+		for(int ii = 1; ii < 4; ii = ii + 1) {
+			printf("T3     Reader        [%d]\n", ii);
+		}
+		sem_post(&sem_3);
 	}
-	sem_post(&sem_3);
 }
 
 void* T4 (void* a) {
-	sem_wait(&sem_3);
-	for(int ii = 1; ii < 5; ii = ii + 1) {
-		printf("T4     Reader        [%d]\n", ii);
+	while(TRUE) {
+		sem_wait(&sem_3);
+		for(int ii = 1; ii < 5; ii = ii + 1) {
+			printf("T4     Reader        [%d]\n", ii);
+		}
+		sem_post(&sem_4);
 	}
-	sem_post(&sem_4);
 }
 
 int main(int argc, char * argv[])
@@ -55,8 +63,6 @@ int main(int argc, char * argv[])
    daftar_trit(T3);
    daftar_trit(T4);
 
-   while(TRUE) {
-   	jalankan_trit();
-   }
+   jalankan_trit();
    beberes_trit("Selesai...");
 }

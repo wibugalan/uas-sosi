c060d68966617ae63c8e1dc1a930adb365c4378e
muhammad.marsha <marshammp@gmail.com>
2021-06-24 11:56:08 +0700

    merge w06 age

--- templateUASSOSI2021/Week06/w06a-simpleparentchild.c
+++ templateUASSOSI2021/Week06/w06a-simpleparentchild.c
@@ -10,6 +10,96 @@ void main(void) {
    rPID=getpid()-id1000+offset;
    rPPID=getppid()-id1000+offset;
    if (rPPID < 1000 || rPPID > rPID) rPPID=999;
-   printf("PID[%d] \t PPID[%4d]\n", rPID, rPPID);
+   if(fork() == 0){
+//      1001
+                rPID=getpid()-id1000+offset;
+                rPPID=getppid()-id1000+offset;
+                printf("PID[%d] \t PPID[%4d]\n",rPID,rPPID);
+                if(fork() == 0){
+//      1002
+                        rPID=getpid()-id1000+offset;
+                        rPPID=getppid()-id1000+offset;
+                        printf("PID[%d] \t PPID[%4d]\n",rPID,rPPID);
+                        if(fork() == 0){
+//      1002                    
+                                if(fork() == 0){
+                                        rPID=getpid()-id1000+offset;
+                                        rPPID=getppid()-id1000+offset;
+                                        printf("PID[%d] \t PPID[%4d]\n",rPID,rPPID);
+                                }
+                                
+                        }
+
+                }
+                else{
+                        if(fork() == 0){
+        //      1003
+                        rPID=getpid()-id1000+offset;
+                        rPPID=getppid()-id1000+offset;
+
+                        printf("PID[%d] \t PPID[%4d]\n",rPID,rPPID);
+                                
+
+                        }
+                }
+   }
+   else{
+        wait(NULL);
+//1000
+
+        if(fork()==0){
+                if(fork()==0){
+//1005
+                        rPID=getpid()-id1000+offset;
+                        rPPID=getppid()-id1000+offset;
+                        printf("PID[%d] \t PPID[%4d]\n",rPID,rPPID);
+                }
+                else{
+//1004
+                        wait(NULL);
+                        rPID=getpid()-id1000+offset;
+                        rPPID=getppid()-id1000+offset;
+                        printf("PID[%d] \t PPID[%4d]\n",rPID,rPPID);
+                        if(fork()==0){
+//1006
+                                rPID=getpid()-id1000+offset;
+                                rPPID=getppid()-id1000+offset;
+                                printf("PID[%d] \t PPID[%4d]\n",rPID
+,rPPID);
+
+}
+                }
+        }
+        else{
+                wait(NULL);
+//1000
+        printf("PID[%d] \t PPID[%4d]\n", rPID, rPPID);
+        if(fork()==0){
+                if(fork()==0){
+//1005
+                        rPID=getpid()-id1000+offset;
+                        rPPID=getppid()-id1000+offset;
+                        printf("PID[%d] \t PPID[%4d]\n",rPID,rPPID);
+                }
+                else{
+//1004
+                        wait(NULL);
+                        rPID=getpid()-id1000+offset;
+                        rPPID=getppid()-id1000+offset;
+                        printf("PID[%d] \t PPID[%4d]\n",rPID,rPPID);
+                        if(fork()==0){
+//1006
+                                rPID=getpid()-id1000+offset;
+                                rPPID=getppid()-id1000+offset;
+                                printf("PID[%d] \t PPID[%4d]\n",rPID
+,rPPID);
+
+}
+                }
+        }
+        
+}
+   }
    fflush(NULL);
+   return;
 }
--- templateUASSOSI2021/Week06/w06b-sortedparentchild.c
+++ templateUASSOSI2021/Week06/w06b-sortedparentchild.c
@@ -5,13 +5,87 @@
 
 void main(void) {
 
-   int rPID, rPPID, id1000=getpid(), offset=1000;
-   
-
-   rPID=getpid()-id1000+offset;
-   rPPID=getppid()-id1000+offset;
-   if (rPPID < 1000 || rPPID > rPID) rPPID=999;
-   printf("PID[%d] \t PPID[%4d]\n", rPID, rPPID);
-   fflush(NULL);
-   return;
+  int rPID, rPID1, rPPID, id1000=getpid(), offset=1000, temp, temp2, temp3, temp4, temp5, temp6;
+  
+  
+  if(fork()>0){//999
+      rPID=getpid()-id1000+offset;
+      rPPID=getppid()-id1000+offset;
+      if (rPPID < 1000 || rPPID > rPID) rPPID=999;
+      sleep(9);
+      printf("PID[%d] \t PPID[%4d]\n", rPID, rPPID);
+      fflush(NULL);
+      }else{
+         if(fork()>0){//1000
+             if(fork()>0){//1001
+                 rPID=getpid()-id1000+offset;
+                 rPPID=getppid()-id1000+offset;
+                 sleep(8);
+                 printf("PID[%d] \t PPID[%4d]\n", rPID, rPPID);
+             }else{
+                if(fork()>0){
+                    if(fork()>0){
+                        rPID=getpid()-id1000+offset;
+                        rPPID=getppid()-id1000+offset;
+                        sleep(6);
+                        printf("PID[%d] \t PPID[%4d]\n", rPID, rPPID);
+                      }else{
+                        if(fork()>0){
+                            if(fork()>0){
+                                rPID=getpid()-id1000+offset;
+                                rPPID=getppid()-id1000+offset;
+                                sleep(4);
+                                printf("PID[%d] \t PPID[%4d]\n", rPID, rPPID);   
+                            }else{
+                               if(fork()>0){
+                                    if(fork()>0){
+                                        rPID=getpid()-id1000+offset;
+                                        rPPID=getppid()-id1000+offset;
+                                        sleep(2);
+                                        printf("PID[%d] \t PPID[%4d]\n", rPID, rPPID);    
+                                    }else{
+                                        if(fork()>0){
+                                            if(fork()>0){
+                                                rPID=getpid()-id1000+offset;
+                                                rPPID=getppid()-id1000+offset;
+                                                printf("PID[%d] \t PPID[%4d]\n", rPID, rPPID);   
+                                            }
+                                        }else{
+                                              rPID=getpid()-id1000+offset;
+                                             rPPID=getppid()-id1000+offset;
+                                             printf("PID[%d] \t PPID[%4d]\n", rPID, rPPID);
+                                        }
+                                    }
+                               }else{
+                                 rPID=getpid()-id1000+offset;
+                                rPPID=getppid()-id1000+offset;
+                                sleep(1);
+                                printf("PID[%d] \t PPID[%4d]\n", rPID, rPPID);
+                               }
+                            }
+                         }else{
+                             rPID=getpid()-id1000+offset;
+                            rPPID=getppid()-id1000+offset;
+                            sleep(3);
+                            printf("PID[%d] \t PPID[%4d]\n", rPID, rPPID);
+                         } 
+                      }
+                 }else{
+                     rPID=getpid()-id1000+offset;
+                    rPPID=getppid()-id1000+offset;
+                    sleep(5);
+                    printf("PID[%d] \t PPID[%4d]\n", rPID, rPPID); 
+                 }
+             }
+         }else{
+            rPID=getpid()-id1000+offset;
+            rPPID=getppid()-id1000+offset;
+            sleep(7);
+            printf("PID[%d] \t PPID[%4d]\n", rPID, rPPID);  
+         }
+    }
 }
+              
+    
+       
+              
